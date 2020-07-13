ba59aec5390aee6da7b57bbc92f2400f0c426f37
kolaczyn <kolaczyn@protonmail.com>
2020-07-13 22:51:42 +0200

    Only dropdown menu left to do

---
+++ script.js
@@ -0,0 +1,4 @@
+let dismissButton = document.getElementById("dismiss-button");
+let whatIs = document.getElementById("what-is-4chan");
+
+dismissButton.addEventListener("click", () => (whatIs.style.display = "none"));
